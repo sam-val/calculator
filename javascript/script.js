d38a36ad2c4378e2565d6bbda188419808d48d01
Sam Val <samvalentine2811@gmail.com>
2021-01-03 23:15:45 +0700

    update js app

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,30 @@
-const my_grid = document.getElementsByClassName("cal-grid")[0];
+const my_grid = document.querySelector(".cal-grid");
+
+
+const current_op = document.querySelector(".current-op");
+
+const previous_op = document.querySelector(".previous-op");
+
 
 
 my_grid.addEventListener('click', (event) => {
     const nodeName = event.target.nodeName;
     if (nodeName == "BUTTON") {
-        console.log(event.target.innerHTML);
+        var btn = event.target.innerHTML;
+        if (isNaN(btn)) {
+
+        } else {
+            console.log(btn);
+            if (current_op.innerHTML.trim() == "0") {
+            current_op.innerHTML = btn;
+            }else {
+                current_op.innerHTML += btn;
+            }
+
+        }
     }
 } )
+
+function hello() {
+    console.log("number 77777777");
+}
