5faf0d4475c54a3c2c35de0597db8697ce6e989f
Sam Val <samvalentine2811@gmail.com>
2021-01-04 00:42:08 +0700

    update script

--- javascript/script.js
+++ javascript/script.js
@@ -11,11 +11,18 @@ var just_finished = false;
 my_grid.addEventListener('click', (event) => {
     const nodeName = event.target.nodeName;
     if (nodeName == "BUTTON") {
+        if (just_finished) {
+            previous_op.textContent = "ans = " + prev;
+        }
         var btn = event.target.innerHTML.trim();
         var current = current_op.textContent;
+        var last_word = current.split(" ");
+        last_word = last_word[last_word.length - 1];
+        console.log("last word", last_word)
         if (isNaN(btn)) {
+            just_finished = false;
             if (btn == ".") {
-                if (current.split(".").length - 1 < 1) {
+                if (last_word.split(".").length - 1 < 1) {
                     current_op.textContent += "."; 
                 }
                 return;
@@ -34,6 +41,11 @@ my_grid.addEventListener('click', (event) => {
             // if operators:
             if (["x","\u00f7", "-", "+", "="].includes(btn))
             {
+                if (prev) {
+                    if (current == "0") {
+                        current_op.textContent = prev;
+                    }
+                }
                 if (btn == "+") {
                     // check if an operator precedes it:
                     
@@ -54,25 +66,35 @@ my_grid.addEventListener('click', (event) => {
             }
 
         } else { // if a number:
-
+            
             if (current_op.innerHTML.trim() == "0") {
             current_op.innerHTML = btn;
             }else if (just_finished){
-                just_finished = false;
                 current_op.innerHTML = btn;
+                just_finished = false;
             }else {
                 current_op.innerHTML += btn;
             }
 
         }
+        
     }
 } );
 
 function my_eval() {
-    var rs = eval(current_op.textContent);
+    let expression = current_op.textContent;
+    try {
+        var rs = eval(expression);
+    // is result is the same as expression:
+    if (rs == expression) {
+        throw "Expresion is unvalid";
+    }
+    } catch (e) {
+        return;
+    }
     current_op.textContent = rs;
     prev = rs;
-    previous_op.textContent = "ans = " + prev;
+    previous_op.textContent = expression + " =";
     just_finished = true;
 }
 
@@ -83,4 +105,3 @@ function sub() {
     
 }
 
-fu
